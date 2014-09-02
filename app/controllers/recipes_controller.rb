d69cf621d4b336685c72411e33ae511714d836dc
Jeremy Whitaker & Randy Leighton <student@epicodus.com>
2014-09-02 14:43:20 -0700

    add update/edit and error check for invalid entry on update.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,27 +3,46 @@ class RecipesController < ApplicationController
   def index
     @recipe = Recipe.new
     @recipes = Recipe.all
-    render('recipes/index.html.erb')
+    render("recipes/index.html.erb")
   end
 
   def create
-
+    @recipes = Recipe.all
+    @recipe = Recipe.new(params[:recipe])
+    if @recipe.save
+      flash[:notice] = "Entered Successfully."
+      redirect_to("/recipes")
+    else
+      render("recipes/index.html.erb")
+    end
   end
 
   def show
-
+    @recipes = Recipe.all
+    @recipe = Recipe.find(params[:id])
+    render("recipes/show.html.erb")
   end
 
   def edit
+    @recipes = Recipe.all
+    @recipe = Recipe.find(params[:id])
+    render("recipes/edit.html.erb")
 
   end
 
   def update
-
+    @recipes = Recipe.all
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(params[:recipe])
+      flash[:notice] = "Updated Successfully."
+      redirect_to("/recipes/#{@recipe.id}")
+    else
+      render("recipes/edit.html.erb")
+    end
   end
 
   def delete
-
+      flash[:notice] = "Removed Successfully."
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   match('/', {via: :get, to: 'recipes#index'})
   match('recipes', {via: :get, to: 'recipes#index'})
-
-
+  match('recipes', {via: :post, to: 'recipes#create'})
+  match('recipes/:id',{via: :get, to: 'recipes#show'})
+  match('recipes/:id/edit', {via: :get, to: 'recipes#edit'})
+  match('recipes/:id/', {via: [:patch, :put], to: 'recipes#update'})
 end

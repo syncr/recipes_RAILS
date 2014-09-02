1e50746695c3ed464f29d009b4fa90364886b69e
Jeremy Whitaker & Randy Leighton <student@epicodus.com>
2014-09-02 10:07:22 -0700

    add tags table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140902163353) do
+ActiveRecord::Schema.define(version: 20140902170154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20140902163353) do
   create_table "recipes", force: true do |t|
     t.string "name"
     t.string "description"
+  end
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end

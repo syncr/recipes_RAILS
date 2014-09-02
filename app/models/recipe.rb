21794e5fd40409bc40529f40b2c7a1ac0c513945
Jeremy Whitaker & Randy Leighton <student@epicodus.com>
2014-09-02 10:01:36 -0700

    validates uniqueness of each Recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,6 @@
 class Recipe < ActiveRecord::Base
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :description, presence: true
+
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -4,5 +4,6 @@ describe Recipe do
 
 it { should validate_presence_of :name}
 it { should validate_presence_of :description}
+it { should validate_uniqueness_of :name }
 
 end

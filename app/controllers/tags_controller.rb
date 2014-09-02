e2b1000067a99b2b1c6ed93e145f6d2187469d15
Jeremy Whitaker & Randy Leighton <student@epicodus.com>
2014-09-02 15:46:51 -0700

    add routes for tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,3 +1,24 @@
 class TagsController < ApplicationController
 
+  def index
+    @tag = Tag.new
+    @tags = Tag.all
+    render("tags/index.html.erb")
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,17 @@ Rails.application.routes.draw do
   match('/', {via: :get, to: 'recipes#index'})
   match('recipes', {via: :get, to: 'recipes#index'})
   match('recipes', {via: :post, to: 'recipes#create'})
-  match('recipes/:id',{via: :get, to: 'recipes#show'})
+  match('recipes/:id', {via: :get, to: 'recipes#show'})
   match('recipes/:id/edit', {via: :get, to: 'recipes#edit'})
   match('recipes/:id', {via: [:patch, :put], to: 'recipes#update'})
   match('recipes/:id', {via: :delete, to: 'recipes#delete'})
+
+  match('/', {via: :get, to: 'tags#index' })
+  match('tags', {via: :get, to: 'tags#index' })
+  match('tags', {via: :post, to: 'tags#create' })
+  match('tags/:id', {via: :get, to: 'tags#show' })
+  match('tags/:id/edit', {via: :get, to: 'tags#edit' })
+  match('tags/:id', {via: [:patch, :put], to: 'tags#update' })
+  match('tags/:id', {via: :delete, to: 'tags#delete' })
 
 end
